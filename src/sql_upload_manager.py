921297109225fe7f15ba29170a144a914b6491bc
ClaraGallardo <clara.gallardo93@gmail.com>
2023-12-01 17:02:13 +0100

    renombrado .py

---
+++ src/sql_upload_manager.py
@@ -0,0 +1,41 @@
+import mysql.connector
+from sqlalchemy import create_engine, text
+
+import pandas as pd
+
+def conention_to_SQL(user,password,host,database = None):
+
+    conexion = mysql.connector.connect(user = user, password = password, host = host, database = None)
+    cursor = conexion.cursor()
+    
+    return cursor 
+
+def add_table(file,key_type,database,password):
+    
+    try:
+        
+        try:
+            
+            df = pd.read_csv(f'../data/{file}.csv')
+            
+        except:
+            
+            return print(f"Error: couldn't find {file}.csv in ../data/")
+            
+    except:
+        
+        engine = create_engine(f'mysql+mysqlconnector://root:{password}@localhost:3306/{database}', echo=True)
+        
+        df.to_sql(file, con=engine, if_exists='append', index=False) # type: ignore
+
+        if key_type == 'primary':
+            
+            with engine.connect() as con:
+                con.execute(text(f'ALTER TABLE `{file}` ADD PRIMARY KEY (`id`)'))
+                            
+        else:
+            
+            with engine.connect() as con:
+                con.execute(text(f'ALTER TABLE `{file}` ADD FOREIGN KEY (`id`) REFERENCES `id`(`id`)'))
+                
+                
