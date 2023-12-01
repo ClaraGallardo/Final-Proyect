7b7adb3ae91c72f116fc2868035d4690ea504cce
ClaraGallardo <clara.gallardo93@gmail.com>
2023-12-01 18:36:26 +0100

    falllo add_table

--- src/sql_upload_manager.py
+++ src/sql_upload_manager.py
@@ -3,14 +3,7 @@ from sqlalchemy import create_engine, text
 
 import pandas as pd
 
-def conection_to_SQL(user,password,host,database = None):
-
-    conexion = mysql.connector.connect(user = user, password = password, host = host, database = database)
-    cursor = conexion.cursor()
-    
-    return cursor 
-
-def add_table(file,key_type,database,password):
+def add_table(file,key_type,database,password,folder):
     
     try:
         
@@ -20,11 +13,12 @@ def add_table(file,key_type,database,password):
             
         except:
             
-            return print(f"Error: couldn't find {file}.csv in ../data/")
+            return print(f"Error: couldn't find {file}.csv in ../{folder}/")
             
     except:
         
         engine = create_engine(f'mysql+mysqlconnector://root:{password}@localhost:3306/{database}', echo=True)
+        
         
         df.to_sql(file, con=engine, if_exists='append', index=False) # type: ignore
 
@@ -32,10 +26,27 @@ def add_table(file,key_type,database,password):
             
             with engine.connect() as con:
                 con.execute(text(f'ALTER TABLE `{file}` ADD PRIMARY KEY (`id`)'))
-                            
+            print(f"The table {file} has been successfully loaded into SQL with the associated {key_type} key.")
+        
         else:
             
             with engine.connect() as con:
                 con.execute(text(f'ALTER TABLE `{file}` ADD FOREIGN KEY (`id`) REFERENCES `id`(`id`)'))
-                
-                
+            print(f"The table {file} has been successfully loaded into SQL with the associated {key_type} key.")    
+
+
+def conection_to_SQL(usuario, clave, host, database=None):
+    try:
+
+        conexion = mysql.connector.connect(user=usuario, password=clave, host=host, database=database)
+
+        cursor = conexion.cursor()
+
+        print("Conexión a MySQL exitosa")
+        
+        return conexion, cursor
+
+    except mysql.connector.Error as err:
+
+        print(f"Error al conectar a MySQL: {err}")
+        return None, None
