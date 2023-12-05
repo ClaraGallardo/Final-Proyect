20e83a668dd4bb79c7061f2b408cfb2699b61a28
ClaraGallardo <clara.gallardo93@gmail.com>
2023-12-05 13:55:50 +0100

    logo

---
+++ app/app_streamlit.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+from PIL import Image
+import pylab as plt
+import webbrowser
+import base64
+import io
+
+st.set_page_config(page_title='App IndiES',
+                   page_icon = '',
+                   layout = 'centered',
+                   initial_sidebar_state='auto')
